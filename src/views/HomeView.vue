a67f809b143418a92e2c3f6b39eec99bb63e3a53
Alexander Akhtyrtsev <a.akhtyrtsev@gmail.com>
2022-10-02 23:53:12 +0300

    Remove dummy code.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,8 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-// Components
-import HelloWorld from "../components/HelloWorld.vue";
-
 export default defineComponent({
   name: "HomeView",
-
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
