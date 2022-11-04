1f2ad1e47757a2a9d459e34845fa0e90e4a49c2c
Alexander Akhtyrtsev <a.akhtyrtsev@gmail.com>
2022-11-04 16:37:53 +0200

    Add i18n.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import i18n from "@/plugins/i18n";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App)
+    .use(router)
+    .use(store)
+    .use(i18n)
+    .use(vuetify)
+    .mount("#app");
