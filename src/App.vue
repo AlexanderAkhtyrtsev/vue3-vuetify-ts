ef768c847b0d18bcf1c2ee5d39f736004897f07c
Alexander Akhtyrtsev <a.akhtyrtsev@gmail.com>
2022-11-05 04:14:55 +0200

    Introduce basic layout.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        shrink-on-scroll
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ $t('homepage') }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
-      <router-view />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,7 +1,11 @@
 import { createI18n } from "vue-i18n";
 
 const i18n = createI18n({
-  messages: {},
+  messages: {
+    'en-US': {
+      homepage: 'Home page',
+    }
+  },
 });
 
 export default i18n;
