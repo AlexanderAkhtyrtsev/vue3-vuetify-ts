dccf87e11c15b425a1f75d510ce1fccec60390f5
Alexander Akhtyrtsev <a.akhtyrtsev@gmail.com>
2022-11-05 04:49:31 +0200

    Add compiler options.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  chainWebpack: config => {
+    config.module
+        .rule('vue')
+        .use('vue-loader')
+        .tap(options => {
+          options.compilerOptions = {
+            ...options.compilerOptions,
+            isCustomElement: tag => ['v-list-item-group'].includes(tag)
+          }
+          return options
+        })
+  },
+
   transpileDependencies: true,
 
   pluginOptions: {
