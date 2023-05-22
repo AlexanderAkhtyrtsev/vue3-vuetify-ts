3eeca56040b3332ff6d834de0e78319f5a27bab7
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2023-05-23 00:41:24 +0300

    Add lazy loading.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,23 +2,42 @@
   <v-container>
     <v-row>
       <v-col
-          v-for="n in 240"
+          v-for="n in limit"
           :key="n"
-          cols="2"
+          :cols="12 / columnCount"
       >
         <v-card>
-          <img :src="'https://picsum.photos/300?random=' + n" alt="Random image" />
+          <img :src="`https://picsum.photos/seed/seed${n}/800`" style="width: 100%" alt="Random image" />
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import {onBeforeMount, onBeforeUnmount, onMounted, ref, watch} from "vue";
 
-export default defineComponent({
-  name: "HomeView",
-  components: {},
-});
+const limit = ref(12)
+const columnCount = ref(3)
+
+const scrollPosition = ref(0)
+
+const scrollListener = () => {
+  scrollPosition.value = document.documentElement.scrollTop
+}
+
+watch(scrollPosition, (v) => {
+  const max = document.documentElement.scrollHeight - document.documentElement.clientHeight
+  if ((v + 200) >= max)
+    limit.value += columnCount.value * 2
+
+})
+
+onBeforeMount(() => {
+  window.addEventListener('scroll', scrollListener)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', scrollListener)
+})
 </script>
